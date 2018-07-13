aea50e3e12328b3ee9ab767ad35bd9f9d647bcd4
priyawadhwa <priyawadhwa@google.com>
2018-07-13 09:26:33 -0700

    Added ISP validation check to admission webhook (#45)
    * Added logic to admission webhook and unit tests
    
    * Fix unit tests
    
    * review code comments

--- pkg/kritis/admission/admission_test.go
+++ pkg/kritis/admission/admission_test.go
@@ -17,32 +17,167 @@ limitations under the License.
 package admission
 
 import (
+	"fmt"
+	"github.com/grafeas/kritis/pkg/kritis/admission/constants"
+	kritisv1beta1 "github.com/grafeas/kritis/pkg/kritis/apis/kritis/v1beta1"
+	"github.com/grafeas/kritis/pkg/kritis/crd/securitypolicy"
+	"github.com/grafeas/kritis/pkg/kritis/metadata"
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestAdmissionReviewHandler(t *testing.T) {
+const (
+	validImage = "gcr.io/image/digest@sha256:0000000000000000000000000000000000000000000000000000000000000000"
+)
+
+type testConfig struct {
+	mockConfig config
+	httpStatus int
+	allowed    bool
+	status     constants.Status
+	message    string
+}
+
+func Test_BreakglassAnnotation(t *testing.T) {
+	mockPod := func(r *http.Request) (*v1.Pod, error) {
+		return &v1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Annotations: map[string]string{"breakglass": "true"},
+			},
+		}, nil
+	}
+	mockConfig := config{
+		retrievePod: mockPod,
+	}
+	RunTest(t, testConfig{
+		mockConfig: mockConfig,
+		httpStatus: http.StatusOK,
+		allowed:    true,
+		status:     constants.SuccessStatus,
+		message:    constants.SuccessMessage,
+	})
+}
+
+func Test_UnqualifiedImage(t *testing.T) {
+	mockPod := func(r *http.Request) (*v1.Pod, error) {
+		return &v1.Pod{
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Image: "image:tag",
+					},
+				},
+			},
+		}, nil
+	}
+	mockConfig := config{
+		retrievePod: mockPod,
+	}
+	RunTest(t, testConfig{
+		mockConfig: mockConfig,
+		httpStatus: http.StatusBadRequest,
+		allowed:    false,
+		status:     constants.FailureStatus,
+		message:    "image:tag is not a fully qualified image",
+	})
+}
+
+func Test_ValidISP(t *testing.T) {
+	mockISP := func(namespace string) ([]kritisv1beta1.ImageSecurityPolicy, error) {
+		return []kritisv1beta1.ImageSecurityPolicy{
+			{
+				ImageWhitelist: []string{validImage},
+			},
+		}, nil
+	}
+	mockConfig := config{
+		retrievePod:                 mockValidPod(),
+		fetchMetadataClient:         mockMetadata(),
+		fetchImageSecurityPolicies:  mockISP,
+		validateImageSecurityPolicy: securitypolicy.ValidateImageSecurityPolicy,
+	}
+	RunTest(t, testConfig{
+		mockConfig: mockConfig,
+		httpStatus: http.StatusOK,
+		allowed:    true,
+		status:     constants.SuccessStatus,
+		message:    constants.SuccessMessage,
+	})
+}
+
+func Test_InvalidISP(t *testing.T) {
+	mockISP := func(namespace string) ([]kritisv1beta1.ImageSecurityPolicy, error) {
+		return []kritisv1beta1.ImageSecurityPolicy{{}}, nil
+	}
+	mockValidate := func(isp kritisv1beta1.ImageSecurityPolicy, project, image string, client metadata.MetadataFetcher) ([]metadata.Vulnerability, error) {
+		return []metadata.Vulnerability{
+			{
+				Severity: "foo",
+			},
+		}, nil
+	}
+	mockConfig := config{
+		retrievePod:                 mockValidPod(),
+		fetchMetadataClient:         mockMetadata(),
+		fetchImageSecurityPolicies:  mockISP,
+		validateImageSecurityPolicy: mockValidate,
+	}
+	RunTest(t, testConfig{
+		mockConfig: mockConfig,
+		httpStatus: http.StatusBadRequest,
+		allowed:    false,
+		status:     constants.FailureStatus,
+		message:    fmt.Sprintf("found violations in %s", validImage),
+	})
+}
+
+func mockMetadata() func() (metadata.MetadataFetcher, error) {
+	return func() (metadata.MetadataFetcher, error) {
+		return nil, nil
+	}
+}
+
+func mockValidPod() func(r *http.Request) (*v1.Pod, error) {
+	return func(r *http.Request) (*v1.Pod, error) {
+		return &v1.Pod{
+			Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{
+						Image: validImage,
+					},
+				},
+			},
+		}, nil
+	}
+}
+
+func RunTest(t *testing.T, tc testConfig) {
 	// Create a request to pass to our handler.
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
+	// Create mocks
+	original := admissionConfig
+	defer func() {
+		admissionConfig = original
+	}()
+	admissionConfig = tc.mockConfig
 	// Create a ResponseRecorder to record the response.
 	rr := httptest.NewRecorder()
 	handler := http.HandlerFunc(AdmissionReviewHandler)
-
 	handler.ServeHTTP(rr, req)
-
 	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
+	if status := rr.Code; status != tc.httpStatus {
 		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+			status, tc.httpStatus)
 	}
-
 	// Check the response body is what we expect.
-	expected := `{"response":{"uid":"","allowed":true,"status":{"metadata":{},"status":"Success","message":"Successfully admitted."}}}`
+	expected := `{"response":{"uid":"","allowed":%t,"status":{"metadata":{},"status":"%s","message":"%s"}}}`
+	expected = fmt.Sprintf(expected, tc.allowed, tc.status, tc.message)
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
--- pkg/kritis/crd/securitypolicy/securitypolicy.go
+++ pkg/kritis/crd/securitypolicy/securitypolicy.go
@@ -28,8 +28,9 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// ImageSecurityPolicies returns all ISP's in all namespaces
-func ImageSecurityPolicies() ([]v1beta1.ImageSecurityPolicy, error) {
+// ImageSecurityPolicies returns all ISP's in the specified namespaces
+// Pass in an empty string to get all ISPs in all namespaces
+func ImageSecurityPolicies(namespace string) ([]v1beta1.ImageSecurityPolicy, error) {
 	cfg, err := clientcmd.BuildConfigFromFlags("", "")
 	if err != nil {
 		return nil, fmt.Errorf("error building config: %v", err)
@@ -39,7 +40,7 @@ func ImageSecurityPolicies() ([]v1beta1.ImageSecurityPolicy, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error building clientset: %v", err)
 	}
-	list, err := client.KritisV1beta1().ImageSecurityPolicies("").List(metav1.ListOptions{})
+	list, err := client.KritisV1beta1().ImageSecurityPolicies(namespace).List(metav1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("error listing all image policy requirements: %v", err)
 	}
