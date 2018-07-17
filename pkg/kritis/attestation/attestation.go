a4a394e4aed5b6e3be852419e019112b161fad34
Tejal Desai <tejaldesai@google.com>
2018-07-16 22:16:54 -0700

    copy sign

--- pkg/kritis/attestation/attestation.go
+++ pkg/kritis/attestation/attestation.go
@@ -26,15 +26,13 @@ import (
 	"io/ioutil"
 	"strings"
 
-	"golang.org/x/crypto/openpgp/armor"
-
-	_ "crypto/sha256"
-
 	"github.com/grafeas/kritis/pkg/kritis/admission/constants"
 	"github.com/pkg/errors"
 
 	"golang.org/x/crypto/openpgp"
+	"golang.org/x/crypto/openpgp/armor"
 	"golang.org/x/crypto/openpgp/packet"
+	"golang.org/x/crypto/openpgp/s2k"
 )
 
 var pgpConfig = packet.Config{
@@ -67,39 +65,15 @@ func VerifyImageAttestation(pubKeyEnc string, attestationHash string, message st
 	buf := bytes.NewBuffer([]byte(attestation))
 	armorBlock, err := armor.Decode(buf)
 	md, err := openpgp.ReadMessage(armorBlock.Body, keyring, nil, &pgpConfig)
-	// // Create a PgpKey from Encoded Public Key
-	// pgpKey, err := NewPgpKey("", pubKeyEnc)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// buf := bytes.NewBuffer([]byte(attestation))
-	// pkt, err := packet.Read(buf)
-	// if err != nil {
-	// 	fmt.Println("error 1")
-	// 	return err
-	// }
-
-	// sig, ok := pkt.(*packet.Signature)
-	// if !ok {
-	// 	return fmt.Errorf("Not a valid signature")
-	// }
-
-	// hash := sig.Hash.New()
-	// _, err = io.Copy(hash, bytes.NewReader([]byte(message)))
-	// if err != nil {
-	// 	return err
-	// }
 
 	// Verify Signature using the Public Key
-
 	plaintext, err := ioutil.ReadAll(md.UnverifiedBody)
 	if err != nil {
 		return err
 	}
 
 	if string(plaintext) != message {
-		return fmt.Errorf("got: %q, want: %q", plaintext, message)
+		return fmt.Errorf("Signature could not be verified. Got: %q, Want: %q", plaintext, message)
 	}
 	return nil
 }
@@ -113,47 +87,51 @@ func AttestMessage(pubKeyEnc string, privKeyEnc string, message string) (string,
 	// Create a PgpKey from Encoded Public Key
 	pgpKey, err := NewPgpKey(privKeyEnc, pubKeyEnc)
 	if err != nil {
-		fmt.Println("here")
 		return "", errors.Wrap(err, "Error while signing:")
 	}
-	// Sign the Message.
-	signer := createEntityFromKeys(pgpKey.PublicKey(), pgpKey.PrivateKey())
+	// First Create a signer Entitiy from public and private keys.
+	signer, err := createEntityFromKeys(pgpKey.PublicKey(), pgpKey.PrivateKey())
+	if err != nil {
+		return "", errors.Wrap(err, "Error while signing:")
+	}
 
 	b := new(bytes.Buffer)
+	// Armor Decode it.
 	armorWriter, errEncode := armor.Encode(b, openpgp.SignatureType, make(map[string]string))
 	if errEncode != nil {
-		fmt.Println("here 1")
 		return "", errors.Wrap(err, "Error while signing:")
 	}
+	// Finally Sign the Text.
 	w, err := Sign(armorWriter, signer, nil, &pgpConfig)
 	if err != nil {
-		fmt.Println("here 3")
 		return "", errors.Wrap(err, "Error while signing:")
 	}
 
 	_, err = w.Write([]byte(message))
 	if err != nil {
-		fmt.Println("here2")
 		return "", errors.Wrap(err, "Error while signing:")
 	}
+	w.Close()
 	armorWriter.Close()
-	fmt.Println("sig =>>", string(b.Bytes()))
+
 	return base64.StdEncoding.EncodeToString(b.Bytes()), nil
 }
 
-func createEntityFromKeys(pubKey *packet.PublicKey, privKey *packet.PrivateKey) *openpgp.Entity {
+func createEntityFromKeys(pubKey *packet.PublicKey, privKey *packet.PrivateKey) (*openpgp.Entity, error) {
 	currentTime := pgpConfig.Now()
 	uid := packet.NewUserId("", "", "")
+	if uid == nil {
+		return nil, errors.New("user id field contained invalid characters")
+	}
 
-	e := openpgp.Entity{
+	e := &openpgp.Entity{
 		PrimaryKey: pubKey,
 		PrivateKey: privKey,
 		Identities: make(map[string]*openpgp.Identity),
 	}
-	isPrimaryId := false
-
+	isPrimaryId := true
 	e.Identities[uid.Id] = &openpgp.Identity{
-		Name:   uid.Name,
+		Name:   uid.Id,
 		UserId: uid,
 		SelfSignature: &packet.Signature{
 			CreationTime: currentTime,
@@ -167,25 +145,20 @@ func createEntityFromKeys(pubKey *packet.PublicKey, privKey *packet.PrivateKey) 
 			IssuerKeyId:  &e.PrimaryKey.KeyId,
 		},
 	}
-
-	keyLifetimeSecs := uint32(86400 * 365)
-
-	e.Subkeys = make([]openpgp.Subkey, 1)
-	e.Subkeys[0] = openpgp.Subkey{
-		PublicKey:  pubKey,
-		PrivateKey: privKey,
-		Sig: &packet.Signature{
-			CreationTime:              currentTime,
-			SigType:                   packet.SigTypeSubkeyBinding,
-			PubKeyAlgo:                packet.PubKeyAlgoRSA,
-			Hash:                      pgpConfig.Hash(),
-			PreferredHash:             []uint8{8}, // SHA-256
-			FlagsValid:                true,
-			FlagEncryptStorage:        true,
-			FlagEncryptCommunications: true,
-			IssuerKeyId:               &e.PrimaryKey.KeyId,
-			KeyLifetimeSecs:           &keyLifetimeSecs,
-		},
+	err := e.Identities[uid.Id].SelfSignature.SignUserId(uid.Id, e.PrimaryKey, e.PrivateKey, &pgpConfig)
+	if err != nil {
+		return nil, err
 	}
-	return &e
+
+	// Set Config Hash from Config
+	hashID, ok := s2k.HashToHashId(pgpConfig.DefaultHash)
+	if !ok {
+		return nil, fmt.Errorf("tried to convert unknown hash %d", pgpConfig.DefaultHash)
+	}
+	e.Identities[uid.Id].SelfSignature.PreferredHash = []uint8{hashID}
+
+	// Set Config Cipher from Config
+	e.Identities[uid.Id].SelfSignature.PreferredSymmetric = []uint8{uint8(pgpConfig.DefaultCipher)}
+
+	return e, nil
 }
