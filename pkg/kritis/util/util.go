93b15aa77d911b2332cb58b233b1502b801b38eb
Tejal Desai <tejaldesai@google.com>
2018-08-22 15:38:04 -0700

    Fix tests

--- pkg/kritis/util/util.go
+++ pkg/kritis/util/util.go
@@ -47,6 +47,7 @@ func CreateAttestationSignature(image string, pgpSigningKey *secrets.PGPSigningS
 	return attestation.CreateMessageAttestation(pgpSigningKey.PublicKey, pgpSigningKey.PrivateKey, hostStr)
 }
 
+// GetOrCreateAttestationNote returns a note if exists and creates one if it does not exist.
 func GetOrCreateAttestationNote(c metadata.Fetcher, a *v1beta1.AttestationAuthority) (*containeranalysispb.Note, error) {
 	n, err := c.GetAttestationNote(a)
 	if err == nil {
