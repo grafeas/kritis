828e593ef87b5a73a58bac1a7915ae5383ad5bfe
Thomas Stromberg <tstromberg@google.com>
2018-08-22 16:09:51 -0700

    Make resolve test failures easier to debug.
    - Remove Exit & Fatalf calls so that tests don't quietly exit
    - Propagate errors upward
    - Improve error messages
    - Document an important note that these tests are not hermetic (*eek!*)

--- cmd/kritis/kubectl/plugins/resolve/main.go
+++ cmd/kritis/kubectl/plugins/resolve/main.go
@@ -17,7 +17,6 @@ limitations under the License.
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/grafeas/kritis/cmd/kritis/kubectl/plugins/resolve/cmd"
@@ -25,7 +24,7 @@ import (
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
+
 }
