f9d4562ac5285f1c129aaf1e49525f0d1645ec0c
Thomas Stromberg <tstromberg@google.com>
2018-08-30 16:21:16 -0700

    Add run_test flags back to their previous version.

--- integration/run_test.go
+++ integration/run_test.go
@@ -46,9 +46,10 @@ const (
 
 var (
 	gkeZone        = flag.String("gke-zone", "us-central1-a", "gke zone")
-	gkeClusterName = flag.String("gke-cluster-name", "UNSET_CLUSTER_NAME", "name of the integration test cluster")
-	gcpProject     = flag.String("gcp-project", "UNSET_GCP_PROJECT", "the gcp project where the integration test cluster lives")
-	gacCredentials = flag.String("gac-credentials", "UNSET_CREDENTIALS_PATH", "path to gac.json credentials for --gcp-project")
+	gkeClusterName = flag.String("gke-cluster-name", "test-cluster-2", "name of the integration test cluster")
+	gcpProject     = flag.String("gcp-project", "kritis-int-test", "the gcp project where the integration test cluster lives")
+	gacCredentials = flag.String("gac-credentials", "/tmp/gac.json", "path to gac.json credentials for --gcp-project")
+	remote         = flag.Bool("remote", true, "if true, run tests on a remote GKE cluster (currently unused)")
 	cleanup        = flag.Bool("cleanup", true, "cleanup allocated resources on exit")
 )
 
