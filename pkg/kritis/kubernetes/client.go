5b9b4a8b81cccfb859fd4554f7ff99efccfdb069
Thomas Stromberg <tstromberg@google.com>
2018-10-26 09:36:31 -0700

    Add auth plugin import into main-like paths

--- pkg/kritis/kubernetes/client.go
+++ pkg/kritis/kubernetes/client.go
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package kubernetes provides client helper libraries for interfacing with the k8s API
 package kubernetes
 
 import (
@@ -25,6 +26,9 @@ import (
 )
 
 // GetClientset returns a configured Kubernetes client.
+// You may need to import the auth plugins to use this:
+//
+// _ "k8s.io/client-go/plugin/pkg/client/auth"
 func GetClientset() (kubernetes.Interface, error) {
 	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
 	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &clientcmd.ConfigOverrides{})
