ef3913caa52be6bd02e604bb6c34349fffd6ada3
Aysylu Greenberg <aysylu@google.com>
2018-11-02 18:27:40 -0400

    Increased timeout for unit tests to 2m

--- hack/test.sh
+++ hack/test.sh
@@ -17,6 +17,6 @@
 set -ex
 
 echo "Running go tests..."
-go test -cover -v -timeout 60s `go list ./...  | grep -v vendor`
+go test -cover -v -timeout 120s `go list ./...  | grep -v vendor`
 GO_TEST_EXIT_CODE=${PIPESTATUS[0]}
 exit $GO_TEST_EXIT_CODE
