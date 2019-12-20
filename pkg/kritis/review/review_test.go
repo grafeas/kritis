f2681770c3b9c97c048d9d0bc64aa045b06e4091
Nenad Dedic <dedic@google.com>
2019-12-20 14:08:48 -0500

    Add regression test cases for base64 encoded attestations in ISP

--- pkg/kritis/review/review_test.go
+++ pkg/kritis/review/review_test.go
@@ -375,6 +375,28 @@ func TestReviewISP(t *testing.T) {
 			shouldAttestImage: false,
 			shouldErr:         false,
 		},
+		{
+			name:              "regression: vulnz w old non-encoded attestation should handle violations",
+			image:             vulnImage,
+			isWebhook:         true,
+			// Invalid because not base64-encoded.
+			attestations:      []metadata.PGPAttestation{{Signature: sigVuln, KeyID: secFpr}},
+			handledViolations: 1,
+			isAttested:        false,
+			shouldAttestImage: false,
+			shouldErr:         true,
+		},
+		{
+			name:              "regression: no vulnz w old non-encoded attestation should create new attestation",
+			image:             noVulnImage,
+			isWebhook:         true,
+			// Invalid because not base64-encoded.
+			attestations:      []metadata.PGPAttestation{{Signature: sigNoVuln, KeyID: secFpr}},
+			handledViolations: 0,
+			isAttested:        false,
+			shouldAttestImage: true,
+			shouldErr:         false,
+		},
 	}
 	for _, tc := range tests {
 		th := violation.MemoryStrategy{
