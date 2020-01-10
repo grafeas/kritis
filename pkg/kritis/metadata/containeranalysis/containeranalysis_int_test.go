8cdf2a7cee48cd0fc38a8387d0e05df8fcd9a3d1
Nenad Dedic <dedic@google.com>
2020-01-10 17:40:57 -0500

    Fix containeranalysis_int_test to create correct aa

--- pkg/kritis/metadata/containeranalysis/containeranalysis_int_test.go
+++ pkg/kritis/metadata/containeranalysis/containeranalysis_int_test.go
@@ -38,7 +38,7 @@ var (
 func GetAA() *kritisv1beta1.AttestationAuthority {
 	aa := &kritisv1beta1.AttestationAuthority{
 		Spec: kritisv1beta1.AttestationAuthoritySpec{
-			NoteReference: fmt.Sprintf("projects/%s", IntProject),
+			NoteReference: fmt.Sprintf("projects/%s/note/%s", IntProject, IntTestNoteName),
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: IntTestNoteName,
