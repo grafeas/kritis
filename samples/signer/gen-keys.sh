e22cae3d1239526f69d8212764d16239ad4d2de0
Don McCasland <donmccasland@google.com>
2020-02-21 14:25:36 -0800

    Adding sample code

---
+++ samples/signer/gen-keys.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+GPG_OUTPUT="$(gpg --quick-generate-key --yes attestor@example.com)"
+KEY_FINGERPRINT="$(echo $GPG_OUTPUT | sed -n 's/.*\([A-Z0-9]\{40\}\).*/\1/p')"
+gpg --armor --export $KEY_FINGERPRINT > gpg.pub
+gpg --armor --export-secret-keys $KEY_FINGERPRINT > gpg.priv
