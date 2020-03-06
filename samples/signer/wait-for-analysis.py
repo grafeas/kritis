d8718cca859ffda1cdd93c0cc2669cdb35a42896
Don McCasland <donmccasland@google.com>
2020-03-05 18:10:06 -0800

    Adding code to get container digest from registry

--- samples/signer/wait-for-analysis.py
+++ samples/signer/wait-for-analysis.py
@@ -1,3 +1,4 @@
+#!/usr/bin//python
 # Lint as: python3
 """
 Polls container analysis api until named container has been scanned for vulnerabilities or timeout
@@ -6,10 +7,38 @@ import json
 import os
 import sys
 import time
+import re
+
+import requests
 
 from grafeas.grafeas_v1.gapic.enums import DiscoveryOccurrence
 from google.cloud.devtools import containeranalysis_v1
 
+def get_resource_url(resource_url):
+    """Returns a properly formatted resource_url like:
+    https://gcr.io/my-project/my-image@sha256:longhash
+    no matter what format you pass in"""
+    m = re.match(r"(https://|)(?P<registry>gcr.io)/(?P<path>[^:@]*)(@(?P<digest>sha256:[a-f0-9]*)|)(:(?P<tag>.*)|)", resource_url)
+    if m.group('digest') is not None:
+        resource_url = "https://{}/{}@{}".format(m.group('registry'), m.group('path'), m.group('digest'))
+    else:
+        tag = m.group('tag')
+        if tag is None:
+            tag = "latest"
+        registry_api = "https://{}/v2/{}/tags/list".format(m.group('registry'), m.group('path'))
+        r = requests.get(registry_api, headers={'Accept': 'application/vnd.docker.distribution.manifest.v2+json'})
+        manifests = r.json()['manifest']
+        digest = ""
+        for key in manifests.keys():
+            if tag in manifests[key]['tag']:
+                digest = key
+                break
+        if digest == "":
+            raise RuntimeError('could not find digest for image {}'.format(resource_url))
+        resource_url = "https://{}/{}@{}".format(m.group('registry'), m.group('path'), digest)
+
+    return resource_url
+        
 def poll_discovery_finished(resource_url, timeout_seconds, project_id):
     """Returns the discovery occurrence for a resource once it reaches a
     terminal state."""
@@ -29,6 +58,7 @@ def poll_discovery_finished(resource_url, timeout_seconds, project_id):
     discovery_occurrence = None
     while discovery_occurrence is None:
         time.sleep(1)
+        resource_url = get_resource_url(resource_url)
         filter_str = 'resourceUrl="{}" \
                       AND noteProjectId="goog-analysis" \
                       AND noteId="PACKAGE_VULNERABILITY"'.format(resource_url)
@@ -54,7 +84,7 @@ def poll_discovery_finished(resource_url, timeout_seconds, project_id):
 def main(argv):
   if len(argv) > 4:
     raise app.UsageError('Too many command-line arguments.')
-  print poll_discovery_finished(resource_url=argv[1], timeout_seconds=30, project_id=argv[2])
+  print poll_discovery_finished(resource_url=argv[1], timeout_seconds=120, project_id=argv[2])
 
 
 if __name__ == '__main__':
