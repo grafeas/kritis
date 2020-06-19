f75b6449417b6601080caf97002f64c298c924bf
Qifan Pu <pqf@google.com>
2020-06-19 16:19:22 -0400

    Fix.

--- integration/signer/signer_int.sh
+++ integration/signer/signer_int.sh
@@ -29,7 +29,7 @@ cat policy_template.yaml \
 # install jq
 # TODO: bake jq into a custom image
 apt-get install -y -q jq
-# Helper function for encoding url
+# Helper functions
 urlencode() {
     # urlencode <string>
     local LC_COLLATE=C
@@ -44,7 +44,45 @@ urlencode() {
     done
 }
 
-export -f urlencode
+delete_image() {
+    set +ex
+    ARG=$?
+    IMG_TO_DELETE=$1
+    echo "Delete image if uploaded."
+    gcloud container images delete $IMG_TO_DELETE --force-delete-tags \
+      --quiet
+    exit $ARG
+}
+
+delete_occ() {
+    set +ex
+    ARG=$?
+    IMG_DIGEST_URL_TO_DELETE=$1
+    echo "Delete occurrence if created."
+    if [ -n "$IMG_DIGEST_URL_TO_DELETE" ]; then
+          ACCESS_TOKEN=$(gcloud --project ${PROJECT_ID} auth print-access-token)
+          ENCODED_RESOURCE_URL=$(urlencode https://$IMG_DIGEST_URL_TO_DELETE)
+          _OCCURRENCES_TO_CLEANUP=$(curl -X GET \
+                 -H "Content-Type: application/json" \
+                 -H "Authorization: Bearer ${ACCESS_TOKEN}"  \
+                 https://containeranalysis.googleapis.com/v1/projects/${PROJECT_ID}/occurrences?filter=kind%3D%22ATTESTATION%22%20AND%20resourceUrl%3D%22${ENCODED_RESOURCE_URL}%22)
+      if [ "$(echo ${_OCCURRENCES_TO_CLEANUP} | jq length)" -gt 0 ]; then
+        _OCC_NAMES=$(echo ${_OCCURRENCES_TO_CLEANUP} | jq '.occurrences | .[] | .name' | tr -d '"')
+        for _OCC_NAME in ${_OCC_NAMES}; do
+          echo "Delete occurrence ${_OCC_NAME}."
+          curl -X DELETE \
+              -H "Content-Type: application/json" \
+              -H "Authorization: Bearer ${ACCESS_TOKEN}"  \
+              -H "x-goog-user-project: ${PROJECT_ID}" \
+              "https://containeranalysis.googleapis.com/v1/${_OCC_NAME}"
+        done
+      fi
+    fi
+    exit $ARG
+}
+
+export -f delete_image
+export -f delete_occ
 
 #### TEST 1: bypass-and-sign mode ####
 ./tests/test-bypass-and-sign.sh
