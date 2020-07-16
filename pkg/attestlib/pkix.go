2dff1b057208afbfb475b46981410c6d51845287
Trey Ridley <treyridley@google.com>
2020-07-16 17:31:10 -0400

    implement pkix sign

---
+++ pkg/attestlib/pkix.go
@@ -0,0 +1,158 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package attestlib
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/sha512"
+	"crypto/x509"
+	"encoding/asn1"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"github.com/pkg/errors"
+	"math/big"
+)
+
+// hashPayload returns the hash function, the hashed payload and an error.
+func hashPayload(payload []byte, signingAlg SignatureAlgorithm) (crypto.Hash, []byte, error) {
+	switch signingAlg {
+	case RsaSignPkcs12048Sha256, RsaSignPkcs13072Sha256, RsaSignPkcs14096Sha256, RsaPss2048Sha256, RsaPss3072Sha256, RsaPss4096Sha256, EcdsaP256Sha256:
+		hashedPayload := sha256.Sum256(payload)
+		return crypto.SHA256, hashedPayload[:], nil
+	case EcdsaP384Sha384:
+		hashedPayload := sha512.Sum384(payload)
+		return crypto.SHA384, hashedPayload[:], nil
+	case RsaSignPkcs14096Sha512, RsaPss4096Sha512, EcdsaP521Sha512:
+		hashedPayload := sha512.Sum512(payload)
+		return crypto.SHA512, hashedPayload[:], nil
+	default:
+		return 0, nil, errors.New("invalid signature algorithm")
+	}
+}
+
+type pkixSigner struct {
+	PrivateKey         interface{}
+	PublicKeyID        string
+	SignatureAlgorithm SignatureAlgorithm
+}
+
+// NewPkixSigner creates a Signer interface for PKIX Attestations. `privateKey`
+// contains the PEM-encoded private key. `publicKeyID` is the ID of the public
+// key that can verify the Attestation signature.
+func NewPkixSigner(privateKey []byte, publicKeyID string, alg SignatureAlgorithm) (Signer, error) {
+	der, rest := pem.Decode(privateKey)
+
+	if len(rest) != 0 {
+		return nil, errors.New("expected one public key")
+	}
+
+	key, err := x509.ParsePKCS8PrivateKey(der.Bytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "some err")
+	}
+
+	// If no ID id provided one is computed based on the default digest-based URI extracted from the public key material
+	if len(publicKeyID) == 0 {
+		var publicKeyMaterial []byte
+		switch key.(type) {
+		case *rsa.PrivateKey:
+			privKey := key.(*rsa.PrivateKey)
+			publicKeyMaterial, err = x509.MarshalPKIXPublicKey(&privKey.PublicKey)
+			if err != nil {
+				return nil, errors.Wrap(err, "some err")
+			}
+		case *ecdsa.PrivateKey:
+			privKey := key.(*ecdsa.PrivateKey)
+			publicKeyMaterial, err = x509.MarshalPKIXPublicKey(&privKey.PublicKey)
+			if err != nil {
+				return nil, errors.Wrap(err, "some err")
+			}
+		default:
+			return nil, errors.New("unexpected key type")
+		}
+		dgst := sha256.Sum256(publicKeyMaterial)
+		base64Dgst := base64.RawURLEncoding.EncodeToString(dgst[:])
+		publicKeyID = fmt.Sprintf("ni:///sha-256;%s", base64Dgst)
+	}
+	return &pkixSigner{
+		PrivateKey:         key,
+		PublicKeyID:        publicKeyID,
+		SignatureAlgorithm: alg,
+	}, nil
+}
+
+// CreateAttestation creates a signed PKIX Attestation. See Signer for more details.
+func (s *pkixSigner) CreateAttestation(payload []byte) (*Attestation, error) {
+	switch s.SignatureAlgorithm {
+	case RsaSignPkcs12048Sha256, RsaSignPkcs13072Sha256, RsaSignPkcs14096Sha256, RsaSignPkcs14096Sha512:
+		hash, hashedPayload, err := hashPayload(payload, s.SignatureAlgorithm)
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		signature, err := rsa.SignPKCS1v15(rand.Reader, s.PrivateKey.(*rsa.PrivateKey), hash, hashedPayload[:])
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		return &Attestation{
+			PublicKeyID:       s.PublicKeyID,
+			Signature:         signature,
+			SerializedPayload: hashedPayload,
+		}, nil
+	case RsaPss2048Sha256, RsaPss3072Sha256, RsaPss4096Sha256, RsaPss4096Sha512:
+		hash, hashedPayload, err := hashPayload(payload, s.SignatureAlgorithm)
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		signature, err := rsa.SignPSS(rand.Reader, s.PrivateKey.(*rsa.PrivateKey), hash, hashedPayload[:], nil)
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		return &Attestation{
+			PublicKeyID:       s.PublicKeyID,
+			Signature:         signature,
+			SerializedPayload: payload,
+		}, nil
+	case EcdsaP256Sha256, EcdsaP384Sha384, EcdsaP521Sha512:
+		var sigStruct struct {
+			R, S *big.Int
+		}
+		_, hashedPayload, err := hashPayload(payload, s.SignatureAlgorithm)
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		sigStruct.R, sigStruct.S, err = ecdsa.Sign(rand.Reader, s.PrivateKey.(*ecdsa.PrivateKey), hashedPayload[:])
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		signature, err := asn1.Marshal(sigStruct)
+		if err != nil {
+			return nil, errors.Wrap(err, "some err")
+		}
+		return &Attestation{
+			PublicKeyID:       s.PublicKeyID,
+			Signature:         signature,
+			SerializedPayload: payload,
+		}, nil
+	default:
+		return nil, errors.New("unknown signature algorithm")
+
+	}
+}
---
+++ pkg/attestlib/pkix_test.go
@@ -0,0 +1,336 @@
+/*
+Copyright 2020 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package attestlib
+
+import (
+	"testing"
+)
+
+const payload = "good payload"
+
+const rsa2048PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDVV/6DeIAscHkY
+fPaxMyZWZ6ZUyKjyICAhcOF90mYAkkJQbrW8ftkU4k1bBU+JfYcTiILyq4tPdrZb
+mHzU6INjYVd8Pm9kHSXJxc1mhqzKFSULhMWwvTW2CHtbtY3mIEOXDFjXdtyC/v1I
+huujP8L6RLX5yq7gZBeYQctgWDzSlBV7uywFpDerEDQcvSF5cl8zRh7jQDDdoLjf
+SH3Z/ZrkmBjwkSpFO1iCHdVW6rObcvhC9JaIfN8Hb0FcIHQay0UrjjxpiSTZE8jp
+DUuShPs2IKq49UXURidRcqsL4A2hav+XuV0QntXi+mj70QKWddzEDOr+Sep43lZ0
+zqI9ifKrAgMBAAECggEAX3wmuYjxNPKSAFfNwbl26y0w0LmOg+/E8bKn+bx1UsQj
+UgR66oaLekFfBeH2mz7HegKbOSl2DHbBX3V8SX5Xb99dXIpFKLWy9//D0qNsKnYU
+QneGU3gb1gEt1PoJLHo8RhqMmMLmJushSZi5VjNvfLvMBqSkaGHj/Y1VDSXw8v3q
+w25PxoudycauNLW0CKLEd2QuPHyjd/krc4R3364PRuIS524V8f+TG3zbXYP+0el3
+oPxApXHuMcbwSN+SqyHJecYt5LBVjhJNjilG1p9V4FMzeMnDeYLbi6PLkObtCye5
+G8yT2pMIgYztCcbOVrVDPnQ3RqJXuz5r4KHLn9c+QQKBgQD02oHfjSWlLBIOMohh
+A1nb3ffsskF6g8aIKz233sGvCrHDyIACzvgwAhV//crp5xuRftyMk0bDpLKFwcvD
+Z0BXKgPntq1IRzDyZyKggXEp/XjtPFs6HHFovyX7a1N3/1J20hJJ+gEVEDXAhqxv
+88cB+92vFQVbVxotRpuqwEtNaQKBgQDfDkaAeuiX00NXWoymgJRU4hg1vxdcEQ7X
+L7JHtUqF510MWsiZBpHfRAV7EN1ZlvGxdBRk2bbTZwV3+4X89SLJLdhS/3fkix1Z
++h/UFm9HZZxcb2UMj/FTuRdQhj1a7h/l6v4KQ2qVCkxwH4Ih5UvLxdmnIcYpbFoB
+UhuKGYy48wKBgBucIBTzN5F+fLK5JAO8ev998hzwpM2J2nM0XQtExRZV4GGxVC38
+5KOVOJNsLNtfQg6P/ZKkcNBS3AaVKcLo+6pYhIQ2ZyfFT1GmK5NpxTb07BPkQG03
+E9q54zCMk6buVYAsg/9vf3u+jjEdfohJNuGUYxUBC8+W7M66LYd9k4AxAoGAemkd
+K9lH2DpqlH/u/FlIDiVeX/CU40PBPzq/vjaxxTapi+QufjxqxXpQ/67LqyT5Uwd/
+QbFxcibxi10PtTjadEmilDn7FAN8giqQWRZOz4HmA5xmejRsniPPtbNV7JcODmGe
+NQe2cECMnmPMSMnLhPL5dY1FcOozotHqpylKXx0CgYBpaBAZeihs5lCZLG9oyeFD
+ujesvS5UdBomDgcDmmhd8dtZOIKsgGuEgbcG8NROrG0tE38JgJ2UxL9xVTYrEuy7
+TCepA1Coedo2Kx1dnBTyvKBfI/TAsLt+JZ38zymUbvrflMG9c9JlhAtNtxm4MF0B
+oujjRhvcxXGmqIt4OSjdQw==
+-----END PRIVATE KEY-----`
+
+const rsa3072PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIIG/gIBADANBgkqhkiG9w0BAQEFAASCBugwggbkAgEAAoIBgQDXvhyXUSA+fXD/
+ivU5M3I7lWJUS2M9XUGXbFcD/gQ5xXK/O13Ci8muILayvL6BbtZL9itpLoFdNjfb
+x1pju7k5gAeev82IXcGgDNsBgykdTqi0yICUYAOuB68RKPlcpSnj5e6JjF048fum
+qAdpnT5ki+nTy8f7FexjX5aoDdMGwrGQLBOZYZRLL+PYaaXpceBfVLLAkYn1UQj/
+nAAITFfc1gfH45cFLlfKMXMigHEWK4DiDwVEdxPDW66Vf3xG39ZPBON6wx5ZoHuY
+7GArTcCtM6KXXX79aBuijDdX3cHml00y9WORqFge+SNiSP8clG5dQqJ1kA2EFYR7
+Cl0cOQr41drwGXFxuTNw7YO9cZG7WDR6ZUieggQ37/V/Phpv+ZNZQaCz1Bf4g0KG
+VJXcQHPkjqZdtzd8TgRbZb0oBIg8oikXUcFoXnwH/9b5cDLubGG5J+Dbf+aR/hIj
+1lUzzaysGYlpOZGApZtlc1p/jIf5GHIYXUKwE3a4uu3rrtLDG/cCAwEAAQKCAYEA
+n9vlsZXST5uBjEi51g2x2kahmpZVzZV5Uatcer/Q/8G3ec2SduI/qb/hwGUswBmR
+vq2EkyZ7gsb28uovpvDlmZ6thsrWj/Hw1BhSezdl0qqDRZm/9LGEaIKXDR8QTr6v
+YIfyjYctWFGtx1qAS5jRL42g0GgSIZGoHCYrKxE7SaogChUu3Js6V8//g08dOfJA
+cEMBRMFSuSy3fzdnqr5u6FrLXyjbsWIaeN7VUj5cDTJlCJxaqdWWFk6r+WiCfL3D
+4VMHVXfG3yKQg7AnvY0PXyeBP2wNA+B8mdnhHPopFmXsHmearf8f9aSR8MS9g6CV
+Zjr+vTsMWMCBAw/0nTWb+wdBLBEYYGluutHdWEio0Y0wibZLlwTOhDASFucOJ2jK
+4CI3JMDt8s1ObEkwZnnGhVBkynSOqWWceEmUIJNZFAN/K9gbhtREj75U2Bd8Udil
+dIIkUsHv/0KHsnyyuXUzj3DH+5K3QOTyxy/d+LvjEXNMpeb8kaC7ne763r1+SQ95
+AoHBAPHnI8XEPLmQW8W8OkToyYm7JbgY1yIY6/1BZs7hKIzhEZTLQNakoxcC4uNq
+ExWvjpYtzEjutc/LSHqoE/xlz7YWSOkJteo9uvP/8H6DwgDn1Nt0nMsyx/8SF+ys
+z5d3MakaZAZIVl74uDBkiPEmRgdbgv2eSNDNefkOD3T7kPejYZoFY6aYK3BBZLUA
++YiCbIpQhv0GDu24NymtU0AQ1+xSthR2ZvYnIqQGXY+wfES0fpv+9j23m+W4hYVX
+aSwNgwKBwQDkULJRyrSRfVnWwj8JfpuBH5gLADSTOJ7h6MLCx3Wx4Z3NSOFAqgZe
+pFujTNCUljmmoxwmY8AC9ezNLLNHswEkVaOxASYMA6zjN6RYs85gqrMJKomKsdsT
+LRD+Nx6KLVYyNh4c1jnkK+wfe52aPQkpcJhoinVLSViqxpJ9vvf1TLMXY6OQvPtX
+/QIOeK30PsiNQSvSM7bFf2w77D1I1C9G6P6cLLesGaWBqS9aPl1FrrAR6x6dzSyk
+ot6VKbeRAX0CgcEAu0OB7+FgWQ6l3MdAHIRRMGxOYNvp/x1n4uA0ZEbti1HYuGU4
+RnGhR3kWh5JgP9NuAFhn4rXYzYnDDCD9h9CRIZbSzoST7UfoaGdjl60MDINrxo/Z
+qXwgUxeAqdmFwu2k5hHmCO8K3e6RN/U8BeLpJ1zyZL+aAvpuSB1YXek5altyfTda
+02bNidM10OF2SvUcbV61gT6lY+XN3letMNVZMjs9dU3LmxbkvDGpUEkBinZn5P9V
+RKXU69sSkgz+//ldAoHANnjaUpFeS2fwy0BE8er3BvOWvukpogZcQeHatcYI2ovN
+rnC47UijhGkKRAfLq+e7kXpQ+QfLg+lQuhL0IyMeHqEgENcJUMx/ryZTW6Zqkr2n
+QYSYlDKi5gxWhZB5BNaUPh6TSKYD4eBo0YoYq/fr+6sVcbu3CGNTeEswIDgzyJ6o
+C+iF2b586R1AJFu+hQuFpc3qupgD/1LLa/CNHpchQHXOaUxx9emYhzrqRUmjz0BC
+WcQS5etRv2XsXQI6/amxAoHACfr6hDiGDjT2fNLa7g29Wf8XLFHDou3buMmlPIvb
+bKF4Rqp1AzajdAVrxM91YjyNUUbU1EElpZ0oKCrM3DiVN+YtziWtRREQ2CqpVgEi
+6Bj5ATAK3OmsvEpKhdpjN5fcvM8BciDLYoYItqsdBc2dfS8zRR6/3nbU8qFB2xCz
+/Rl8l/RAb0b/GhZwF1qcyYSLVlRQP5AzhSo/d/ZWX/usZIxKUGZ/gTX6KTBH2h39
+iFC0mK345W//6CokZ3In6And
+-----END PRIVATE KEY-----
+`
+
+const rsa4096PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIIJQwIBADANBgkqhkiG9w0BAQEFAASCCS0wggkpAgEAAoICAQC174JYVTA7DIP9
+UFornmirFMw5ffhjd69WUJj2INfBhuL7IOBKCMHgraxO/JXrO1AQFbNF41fcYbta
+Q9aQdUE4iyxGC1VS02eeBCSCg7AlbEZ/STVH82tdGxExF7K5bXNPqJ+1WEKV7F/l
+4Eu2arLuFcKnLs03H3jzpikXwokZrXCtXcqjdHWodiMaAp39+O1DkJwVRlRiCZhB
+4iR4M7UOJA5JhLbGeRRN9XZociAVSWvF1TYznQd7ln+NITj3/0hdBpzn33LabqKt
+VpVPpUy9jqbFJEaBHHeGShqNWvF3cyCUgUs7z8V+s4bBmD1tkUiWAeNSIU88pufx
+tLKV0xnE2Pfq3tgLZmk3pcbN4Gect8FbUaMaiaTcbV7IFQLBGT2ce30Oyqv/ERq4
+yhwdx8AR8Sbz9/BynkwHwY5neXyn1GzfzKgEXNVuPZJ6JwWhBOcd7gOu0y8HMbIq
+yJoqzNXlQkA/KqbvM+mH8KT741FAOYm+SEtdyztCtQbZMEDu96povOtTzyNjdEMp
+Ihs20sDEGuZAgPWaztNWsJwHqdavEclzcWFM/qr+VQvTOIbx840P2hbo/RjFpeP7
+WojGfjnHaPmgPCzHyKDj9Xbdi6h++OLaP9VgBj8oCQok5ep+jNuaUw1QKk3oe5Dn
+JqRUWBZdqcXP32vDiRnTPvTOIIGnrwIDAQABAoICABZZpQaa+IDYicJtw5YGMM+L
+j4ckjYYSpuyQnCnoYPj6TBoTsNoHd2HVlAxkBTrPn4dKUyqrwhhlvTFakhpO/UMn
+/blhHQs+w0TrN1z9Nn9ybLqoUK5VHgRjFdqVEbSr2HWZvnz1qcBnOGSyCDsRooeT
+DsWg/z7dU+uCpxuvH06TOvthQUqRPP8D/E3usmAH24XoVvRaHnWovhah5F8L4xso
+nMS1VArPxwnItNIh6wUSbvXvumfey5OIKoU45pm4t7gN3uqxTZe97agjsdYF9YZ3
+5s7jJPIpgBl0MwzYdj95WjBELezcGmP1RAG2ZR13spYTB00IoIZP7sCHtUSomUqb
+PvcNgu0gm/dxSqSh7Ws3zpIwQeMAEbP8zpZpE9Ihn/LI3tTBsY5Iz0BNJfwkGmfC
+UK/SqQqgofwDVDxpuv0CIGcY8eOMRJ73aX+e2mBVq6bC9nArTZdGWxXtxoC2ykoE
+JotKMvoi2pZOZQ7DgBt96HRJrmGdOEIE+dA8VdhI4bEw5RCApTUZUzgNgYXyKEfZ
+sKnfzmtNhec9a/EWSt1x1sS2FQ1Kdkns0ot3N9tExHEOSLnesK9RxGUv4vAC1sil
+1l6VsEaia/MvgEK8rw/pfbmxFdE3jrUyoLcH/AVeHFoSST9uvoeYpgFO3FAvwGWC
+m6YbINJvigSyP61fXothAoIBAQDoA8AUDtjBYwwSc8RBROxXMp208nItvD7BBxP/
+VlHka8GYJCmDZ6XuImz/FqjCaPoPc4KzawbtwWIRsX2YZM2BcUV2COJ9qee/2ORR
+bP+b0gxUYCJel7DGYdwuUhzVho/Ib+5MAtSwwsVvs93ld5zqjrYkVWDiN7aPRPlZ
+Daz0wdCaKBcYazkJ1Pi1qa5C7QDE8Z1PRsyyOD+6GE4EuoyyO0prp/7G5bn5wa8d
+DQT4y78El3Sjz7yCmnuPDlzm9qptS7KerNEopVoFiwDmRHVUQkXh5y2jcizQz2PX
++g0CXpNUHa9DZGr1hFq/Y41Q9qmuqPlsL/dqFx//qVIGnddxAoIBAQDIvmuUcez9
+1LlJrdpibs6b9i1qs8wh41X0P/Gnpcg5fc1At2CSVJjpDM1CSKrZ0vrcDwOmv1B7
+QvzFEFaIHJEvzUyE82EktJBxTyihm5Tpo6JSaBVh/1QBGWp2JWFEAT7zCftwQ56y
+85fEySSWcw9jFASrljylCQpwW+zvtp/iuv6hsHbRDokHDcSLTON6/iYJFdDfMPrS
+EQBoiKtcS635CLinzVuWkljk5LLSNH8p41AMOlqksNqCNgpsDPAJc+gVoB1fd7/Z
+CkYA+3eAKZtqg2pmiAXDLkHMiOw2gsuWMt7Ipd4k0pXf67wqyWCLCEKXdHLF0D2w
+iEXrbvuLDyEfAoIBAQCZVHkpCFLJIxsESsuFUvZZ/hJpVdyNSx2x1Wqq0TIXofrz
+/9oLhIENKmUcA66NpHC1q2HSDsYqdCXKiaD2CJAjnmcIRYDhCVsl10G0ajNWl+8c
+B9hO6TKjOGMZnZx+pIn7LbWvOCO1mVJ/qr/XeUATZzMKOf5oIeedMTkUWExYCIpC
+hoIU2i98xumCrNuEUTqz7PVIQgLfOPsskgllaJ+43iQll4VxjrpkS1LeAQ+rGINp
+lmo4I/5x1YQYm6Zb4OHqjx9Ba/ZiRTyD80Qp6U4c3Btw2qW3oanqaVSTlCXqzfPR
+oDiGciOZk9mhKDppKkojKnqDvANnclbsT8rson2RAoIBAATQrHvvD6Q3Cgq+ZNKf
+nc0X5pb8oRTfAxzMu9stmtPmNJosy+A2r0RHzlScsZxv9xx4L7o8NI4AWVfQwIKb
+TNA8uG0n+ViMDDrrjynxNW01Q5t+a6TiHv23wln7I44iCYSC5MkYliHsveM2f+4r
+7F6QCFylSecbhGiVi+VkGAm0Oo9eUDtLgfXtWp2MxpgYqIjnQQ9ZeWq4ha38OUAJ
+gK7MisFbp6rA7+jto2xvXe1/SlhAvhPdOiCNc0qMNOXFJff+0zWLBcsK+Arl/UPR
+u6yeYlr7QEgiP+nMDv5vvLpplfZyJBX2BypL2UOdePdcx1wiqu4bxJ8LZcHz84yu
+XDkCggEBAKoMUQDxpOknp/OXGfUHqqQWuA2pIubaaFyL25m4r0TcoHjmjtAig8Ho
+0mt9PLJ+O3U3myb4REvYMN9pqCzTO4phbEzeHHmNdQZrj9PxUReoT470LMxXFdws
+pQ3IR5qtvnPm9yR/lx3Dqjl2zWFlxsaTLVXQZSSRc9dpuSJuSDV7NrNabtRufaA8
+/JL79GIopPVAxvJwP3x7ORSayTBcxaYy3Z8OqJ/mFYAwZLzi1phJtz0jMZZ+9cq1
+UOKpjzKbyb+AwI1Hzs0eitfSMAeKH2Dy+IOtXWv3JPCirb7YQXU/5jpYKiQzhTMo
+sO4nyZVNmEYg4tvJVA81UEmK2twOA/Q=
+-----END PRIVATE KEY-----`
+
+const ec256PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgS0iQ5IlSHswYTbiz
+vwR/YeueAakvrLayzcuLVzUPg+ihRANCAASLomGqGvbwvN3ai5f+kUsBXeBcJD26
+rqfIemztatGwDuBymNVP0fabJscWxObqe3iaER5NOJg167zqA11PQueQ
+-----END PRIVATE KEY-----`
+
+const ec384PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIG2AgEAMBAGByqGSM49AgEGBSuBBAAiBIGeMIGbAgEBBDBXs02405f5G7xKHY/l
+QprUkVyEVWvNEXe9+xDmOD1pHHpnsTlRfJQo1de0ROuk08ChZANiAATIz2wT8EEq
+omyOrPu6hhkR8/0PrY80HSThgHndPFvmvI6vUo6sXS6PJEXPp+iu2GSSYQkL3m+l
+kdBOhfd+fYTPSzZVKs5XulL3TQ0ohrMipX4gG5x0wVMoy+CHkxAMBug=
+-----END PRIVATE KEY-----`
+
+const ec521PrivateKey = `-----BEGIN PRIVATE KEY-----
+MIHuAgEAMBAGByqGSM49AgEGBSuBBAAjBIHWMIHTAgEBBEIBWNVxpOzimyMDpF9b
+bUp3rPyMCbTaLzi3/7MWHcTO6/OIMeNgOU9ShxSnYAaOa+IPa4HQxsmv1uesijTa
+zVKWWOKhgYkDgYYABAH+ih5cBui0i6lcY8fcxHz+0cHx8JPqEg7hWad1JQbux/J4
+PsIC6zJmuxzHlr1V07i+dnLGZPxVbjYstLL3J0BHUgE+upGIIPmULuC/P/0xBHiC
+Q2XPurJc20OMt1Spsg61RpFAs2Zd5rZQEfrji/TeyI40FH6y72jPiYcfY1IcvCaV
+Lg==
+-----END PRIVATE KEY-----`
+
+func TestNewPkixSigner(t *testing.T) {
+	tcs := []struct {
+		name               string
+		privateKey         []byte
+		publicKeyId        string
+		signatureAlgorithm SignatureAlgorithm
+		expectedError      bool
+	}{
+		{
+			name:               "valid RSA 2048 key",
+			privateKey:         []byte(rsa2048PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaSignPkcs12048Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "valid EC key",
+			privateKey:         []byte(ec256PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: EcdsaP256Sha256,
+			expectedError:      false,
+		}, {
+			name:               "valid EC key with no id successful",
+			privateKey:         []byte(rsa2048PrivateKey),
+			publicKeyId:        "",
+			signatureAlgorithm: RsaSignPkcs12048Sha256,
+			expectedError:      false,
+		}, {
+			name:               "invalid private key",
+			privateKey:         []byte("invalid key"),
+			publicKeyId:        "invalid-kid",
+			signatureAlgorithm: RsaSignPkcs12048Sha256,
+			expectedError:      true,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := NewPkixSigner(tc.privateKey, tc.publicKeyId, tc.signatureAlgorithm)
+			if tc.expectedError {
+				if err == nil {
+					t.Errorf("NewPkixSigner(...) = nil, expected non nil")
+
+				}
+			} else {
+				if err != nil {
+					t.Errorf("NewPkixSigner(..) = %v, expected nil", err)
+				}
+			}
+		})
+	}
+}
+
+func TestCreatePkixAttestation(t *testing.T) {
+	tcs := []struct {
+		name               string
+		privateKey         []byte
+		publicKeyId        string
+		signatureAlgorithm SignatureAlgorithm
+		expectedError      bool
+	}{
+		{
+			name:               "create RSA 2048 signature successful",
+			privateKey:         []byte(rsa2048PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaSignPkcs12048Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 3072 signature successful",
+			privateKey:         []byte(rsa3072PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaSignPkcs13072Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 4096 SHA 256 signature successful",
+			privateKey:         []byte(rsa4096PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaSignPkcs14096Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 4096 SHA 512 signature successful",
+			privateKey:         []byte(rsa4096PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaSignPkcs14096Sha512,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 2048 PSS signature successful",
+			privateKey:         []byte(rsa2048PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaPss2048Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 3072 PSS signature successful",
+			privateKey:         []byte(rsa3072PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaPss3072Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 4096 SHA 256 PSS signature successful",
+			privateKey:         []byte(rsa4096PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaPss4096Sha256,
+			expectedError:      false,
+		},
+		{
+			name:               "create RSA 4096 SHA 512 PSS signature successful",
+			privateKey:         []byte(rsa4096PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: RsaPss4096Sha512,
+			expectedError:      false,
+		},
+		{
+			name:               "create EC P256 signature successful",
+			privateKey:         []byte(ec256PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: EcdsaP256Sha256,
+			expectedError:      false,
+		}, {
+			name:               "create EC P384 signature successful",
+			privateKey:         []byte(ec384PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: EcdsaP384Sha384,
+			expectedError:      false,
+		},
+		{
+			name:               "create EC P521 signature successful",
+			privateKey:         []byte(ec521PrivateKey),
+			publicKeyId:        "kid",
+			signatureAlgorithm: EcdsaP521Sha512,
+			expectedError:      false,
+		},
+		{
+			name:               "invalid signature algorithm",
+			privateKey:         []byte(rsa2048PrivateKey),
+			publicKeyId:        "keyId",
+			signatureAlgorithm: 0,
+			expectedError:      true,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			signer, err := NewPkixSigner(tc.privateKey, tc.publicKeyId, tc.signatureAlgorithm)
+			if err != nil {
+				t.Fatalf("failed to create signer")
+			}
+			attestation, err := signer.CreateAttestation([]byte(payload))
+			if tc.expectedError {
+				if err == nil {
+					t.Errorf("CreateAttestation(...) = nil, expected non nil")
+
+				}
+			} else {
+				if err != nil {
+					t.Errorf("CreateAttestation(..) = %v, expected nil", err)
+				} else if attestation.PublicKeyID != tc.publicKeyId {
+					t.Errorf("attestation.PublicKeyID = %v, expected %v", attestation.PublicKeyID, tc.publicKeyId)
+				}
+			}
+		})
+	}
+}
