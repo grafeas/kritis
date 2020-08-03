cb78f6294944561d29232445ba615dd8584df8c2
treyridley <59669639+treyridley@users.noreply.github.com>
2020-08-03 11:34:52 -0400

    Update pkix key parser (#576)
    * update pkix key parser
    
    * fix format
    
    * fix vendor error
    
    * pull key cases out as const
    
    * wrap error
    
    * wrap errors

--- pkg/attestlib/pkix_key.go
+++ pkg/attestlib/pkix_key.go
@@ -27,18 +27,38 @@ import (
 	"github.com/pkg/errors"
 )
 
+const rsaPkcs1Key = "RSA PRIVATE KEY"
+const ecPkcs1Key = "EC PRIVATE KEY"
+const pkcs8Key = "PRIVATE KEY"
+
 func parsePkixPrivateKeyPem(privateKey []byte) (interface{}, error) {
 	der, rest := pem.Decode(privateKey)
 
 	if len(rest) != 0 {
 		return nil, errors.New("expected one public key")
 	}
-
-	key, err := x509.ParsePKCS8PrivateKey(der.Bytes)
-	if err != nil {
-		return nil, err
+	switch der.Type {
+	case rsaPkcs1Key:
+		key, err := x509.ParsePKCS1PrivateKey(der.Bytes)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse rsa pkcs1 private key")
+		}
+		return key, nil
+	case ecPkcs1Key:
+		key, err := x509.ParseECPrivateKey(der.Bytes)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse ecdsa pkcs1 private key")
+		}
+		return key, nil
+	case pkcs8Key:
+		key, err := x509.ParsePKCS8PrivateKey(der.Bytes)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to parse pkcs8 private key")
+		}
+		return key, nil
+	default:
+		return nil, errors.New("unexpected key type")
 	}
-	return key, nil
 }
 
 func generatePkixPublicKeyId(privateKey interface{}) (string, error) {
